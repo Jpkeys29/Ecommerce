5338ff2f5c589e34a834a3b8070c12864c24c99b
Jpkeys29 <jpmusik29@gmail.com>
2023-03-10 08:08:14 -0500

    ajax integration add

--- cart/cart.py
+++ cart/cart.py
@@ -4,7 +4,7 @@ class Cart():
         #Creating a session:
         self.session = request.session
 
-        #Returning customer - obtain their existing session
+        #Returning customer - obtain their existing session. 'session_key' could be any other name
         cart = self.session.get('session_key')
 
         #New user - create a new session
