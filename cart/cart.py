8adbc07c33988b58f45391aa157bdccc656699b2
Jpkeys29 <jpmusik29@gmail.com>
2023-03-10 09:39:40 -0500

    functionality pt.1

--- cart/cart.py
+++ cart/cart.py
@@ -14,4 +14,16 @@ class Cart():
         #To have access to our cart from any page:
         self.cart = cart
 
+    # It comes from the function cart.add from views
+    def add(self,product,product_qty):
+        product_id = str(product.id)
+
+        # performing a test. If product is in the cart:
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = product_qty  #modifies the quantity of product
+        else: #if the product does not exits in the user's car
+            self.cart[product_id] = {'price': str(product.price), 'qty': product_qty} #then take the price along with quantity
+
+        self.session.modified = True #Just telling Django that the session has been modified
+
             
--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+from .cart import Cart
+from store.models import Product
+from django.shortcuts import get_object_or_404
 
 def cart_summary(request):
     return render(request, 'cart/cart-summary.html')
+
 def cart_add(request):
-    pass
+    cart = Cart(request)
+
+    # To check if we are getting a AJAX request:
+    if request.POST.get('action') == 'POST':
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+        #Getting a object from the model 'product' with id from product id from ajax request
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product, product_qty = product_quantity)
+
+
 def cart_update(request):
     pass
+
 def cart_delete(request):
     pass
 
